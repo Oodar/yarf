0c60337a27d682386003a00536a8824b0190f24e
Matthew Hewitson <matthewh@crytek.com>
2013-05-01 11:20:25 +0100

    Initial commit for Controller

--- src/yarf/Controller.php
+++ src/yarf/Controller.php
@@ -4,7 +4,37 @@ namespace yarf;
 
 class Controller
 {
+    protected $respondTypes = array();
+
     function __construct()
     {
+    }
+
+    public function respondTo($type, $function)
+    {
+        // you'd still have to actually get the collection passed into the closure somehow
+        // this has to happen here, magically
+        assert('is_callable($function)');
+        if($this->matchesAcceptType($type)) {
+            // call function
+            return call_user_func($function);
+        } else {
+            // don't call function
+            return false;
+        }
+    }
+
+    private function matchesAcceptType($type)
+    {
+        $accept = Environment::getAcceptedTypes();
+
+        foreach ($accept as $acceptType) {
+
+            if(strstr($acceptType, $type)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
