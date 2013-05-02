9f703014fa8ddbfd36ed5bcf0d152115457c130c
Matthew Hewitson <matthewh@crytek.com>
2013-05-02 12:03:24 +0100

    Initial commit

---
+++ src/yarf/http/Response.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace yarf\http;
+
+class Response
+{
+    protected static $responses = array(
+        #1xx = information
+        #2xx = success
+        200 => "200 OK",
+        #3xx = redirect
+        #4xx = client error
+        404 => "404 Not Found",
+        #5xx = server error
+        500 => "500 Server Error"
+    );
+
+    private $status;
+    private $headers = array();
+    private $body;
+
+    function __construct()
+    {
+    }
+
+
+    public function setResponseCode($code)
+    {
+        if(array_key_exists($code, self::$responses)) {
+            // a supported, valid code
+            $this->status = self::$responses[$code];
+        } else {
+            throw new \Exception("Unsupported response code");
+        }
+    }
+
+    public function getResponseCode()
+    {
+        return $this->status;
+    }
+
+    public function setHeader($header, $value)
+    {
+        $this->headers[$header] = $value;
+    }
+
+    public function getHeader($header)
+    {
+        return $this->headers[$header];
+    }
+
+    public function sendHeaders()
+    {
+        // send status line
+        header('HTTP/1.1 ' . $this->status);
+
+        foreach ($this->headers as $header => $value) {
+            header($header . ': ' . $value);
+        }
+    }
+}
